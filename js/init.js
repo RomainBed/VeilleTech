4d7da49ff6a3f13210075764a8322d3bacd4e875
RomainBed <romainbedouret@gmail.com>
2022-12-28 11:14:00 +0100

    Finalisation page JS et arrangement code

--- js/init.js
+++ js/init.js
@@ -1,26 +1,13 @@
 (function($){
   $(function(){
-
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery); 
 
 
 $(document).ready(function(){
   $('.sidenav').sidenav();
-
-
-// SWAP ICON ON CLICK
-// Source: https://stackoverflow.com/a/34254979/751570
-$('.dark-toggle').on('click',function(){
-  if ($(this).find('i').text() == 'brightness_4'){
-      $(this).find('i').text('brightness_high');
-  } else {
-      $(this).find('i').text('brightness_4');
-  }
-});
 });
 
 $(document).ready(function(){
